3654648aa156b58480f531b64d7a2e9788f94281
Joe <modulusman@gmail.com>
2019-01-17 17:42:04 -0800

    sets up basic question objects for each topic

--- js/main.js
+++ js/main.js
@@ -3,3 +3,32 @@ var subjects = document.querySelector('#topics');
 var selected = subjects.addEventListener('click', function (e) {
   document.getElementById('page_title').innerHTML = e.target.innerText + ' Questions';
 });
+
+// basic water distribution questions
+var distro_questions = {
+  'How many turns per inch of pipe to close a valve?'     : '3 turns',
+  'What does CMLC stand for?'                             : 'cement mortar lined concrete',
+  'What schedule PVC pipe is common in households?'       : 'schedule 40',
+}
+
+// basic water treatment questions
+var treat_questions = {
+  'What is the chemical formula for sodium hypochlorite?' : 'naocl',
+  'What does a pH of 7 mean on the pH scale?'             : 'neutral',
+  'What type of gas was commonly used for disinfection?'  : 'chlorine',
+}
+
+// basic web dev questions
+var web_questions = {
+  'What does HTML stand for?'                             : 'hyper text markup language',
+  'What does HTTPS mean?'                                 : 'hyper text transfer protocol secure',
+  'Semantic is a term used to describe what?'             : 'html',
+  'What programming language does Rails utilize?'         : 'ruby',
+  'SQL is used to store information in a what?'           : 'database',
+}
+
+// basic it questions
+var it_questions = {
+  'What is DNS?'                                          : 'domain name service',
+  'What command will filter a search in Linux?'           : 'grep',
+}
